01981bae1cba36856ff4fca9928886fb82339590
JuanDaniel00 <juanuribe@colegioguanenta.edu.co>
2024-06-20 11:13:53 -0500

    chore: Update contacto.vue component with contact form and location map

--- src/components/contacto.vue
+++ src/components/contacto.vue
@@ -1,16 +1,20 @@
 <template>
-  <q-page class="q-pa-md">
-    <div class="row justify-center">
-      <div class="col-md-8 col-sm-10 background">
-        <div class="q-mt-xl">
+  <q-page class="q-pa-md custom-padding">
+    <div class="row justify-center custom">
+      <div class="col-md-8 col-sm-10 background  ">
+        <div class="q-mt-xl ">
           <h3 class="text-h5 q-mb-md">Ubicación</h3>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3963.8355511546097!2d-73.12894414610234!3d6.542440122696085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sco!4v1718741661248!5m2!1ses-419!2sco"
-            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <div class="q-pa-md">
+            <div class="map-container">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3963.8355511546097!2d-73.12894414610234!3d6.542440122696085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sco!4v1718741661248!5m2!1ses-419!2sco"
+                width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+          </div>
         </div>
 
-        <div class="q-mb-lg contFormData">
+        <div class="q-mb-lg contFormData ">
           <div class="q-mb-lg data">
             <h2 class="text-h4 q-mb-md">Contacto</h2>
             <p class="text-subtitle1">
@@ -35,98 +39,135 @@
   </q-page>
 </template>
 
-<script>
+<script setup>
 import { ref } from 'vue';
 import Swal from 'sweetalert2';
 
+const name = ref('');
+const email = ref('');
+const message = ref('');
 
-export default {
-  setup() {
-    const name = ref('');
-    const email = ref('');
-    const message = ref('');
+const submitForm = () => {
+  if (!name.value || !email.value || !message.value) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: 'Por favor, complete todos los campos.',
+    });
+    return;
+  }
 
-    const submitForm = () => {
+  Swal.fire({
+    icon: 'success',
+    title: 'Mensaje enviado',
+    text: 'Gracias por contactarnos. Nos pondremos en contacto con usted lo antes posible.',
+  });
 
-      if (!name.value || !email.value || !message.value) {
-        Swal.fire({
-          icon: 'error',
-          title: 'Error',
-          text: 'Por favor, complete todos los campos.',
-        });
-        return;
-      }
-
-      Swal.fire({
-        icon: 'success',
-        title: 'Mensaje enviado',
-        text: 'Gracias por contactarnos. Nos pondremos en contacto con usted lo antes posible.',
-      });
-
-      name.value = '';
-      email.value = '';
-      message.value = '';
-
-    };
-
-    return { name, email, message, submitForm };
-  },
+  name.value = '';
+  email.value = '';
+  message.value = '';
 };
 </script>
 
 <style scoped>
-
-.contFormData{
+.contFormData {
   display: flex;
   flex-wrap: wrap;
-  justify-content: end;
+  justify-content: space-around;
   align-items: center;
 }
 
-.data{
-  width: 50%;
+.map-container {
+  position: relative;
+  width: 100%;
+  padding-top: 56.25%;
+  /* 16:9 Aspect Ratio */
 }
 
-.form{
-  width: 50%;
+.map-container iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 
-.background{
+.data {
+  width: 40%;
+  height: 100%;
+  border-radius: 10px;
+  background-color: #fffae0;
+  order: 1;
+}
+
+.form {
+  width: 40%;
+  border-radius: 10px;
+  background-color: #90e3be;
+  order: 2;
+  padding: 20px;
+  margin-top: 20px;
+}
+
+.background {
   background-color: #f5f5f5;
   padding: 20px;
   border-radius: 10px;
 }
 
-.text-h4{
+.text-h4 {
   color: #2e5845;
   font-weight: bold;
   margin-bottom: 20px;
   text-align: center;
 }
 
-.text-h5{
+.text-h5 {
   color: #2e5845;
   margin-bottom: 8px;
   font-weight: bolder;
 }
 
-.text-subtitle1{
+.text-subtitle1 {
   color: #333;
   margin-bottom: 8px;
 }
 
-.text-caption{
+.text-caption {
   color: #5D4037;
   font-weight: bolder;
   font-size: medium;
 }
 
-.btn{
+.btn {
   background-color: #2e5845 !important;
   color: #fffae0;
   margin-top: 16px;
 }
 
+@media (max-width: 768px) {
+  .contFormData {
+    flex-direction: column;
+  }
 
+  .data {
+    width: 100%;
+  }
 
+  .form {
+    width: 100%;
+  }
+
+  .map-container {
+    width: 100%;
+    height: 300px;
+  }
+}
+
+@media (max-width: 600px) {
+  .custom>.col-sm-10 {
+    height: auto;
+    width: 90% !important;
+  }
+}
 </style>
